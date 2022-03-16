d20b5da38763122502d8186e668286efdf54381a
Vishal <dev@v-empire.digital>
2022-03-16 21:23:08 +0530

    Update deploy script

--- nft-plebeian-council/scripts/deploy.js
+++ nft-plebeian-council/scripts/deploy.js
@@ -7,43 +7,42 @@ async function main() {
 
   // Loading the contract before deploying.
   const tokenContract = await ethers.getContractFactory("NFT");
+  const saleContract = await ethers.getContractFactory("Sale");
+  const stakeToken = await ethers.getContractFactory("StakeNFT");
+  const erc20Contract = await ethers.getContractFactory("SampleToken");
+
+  // Deploy NFT
   const nft = await upgrades.deployProxy(
     tokenContract,
     ["Test", "Test", 4500],
     { kind: "uups" }
   );
   await nft.deployed();
-  // Print the address of deployed contract
-  console.log("Contract deployed to:", nft.address);
+  console.log("NFT deployed to:", nft.address);
 
-  // Loading the contract before deploying.
-  const saleContract = await ethers.getContractFactory("Sale");
-  // Deploy proxy.
+  // Deploy Sale
   const sale = await upgrades.deployProxy(
     saleContract,
     [nft.address, 100, 100, 100, 100, 100],
     { kind: "uups" }
   );
-  // Waiting till the transaction is completed.
   await sale.deployed();
-  // Print the address of deployed contract
   console.log("Sale deployed to:", sale.address);
 
-  const stakeToken = await ethers.getContractFactory("StakeNFT");
-  const erc20Contract = await ethers.getContractFactory("SampleToken");
-
+  // Deploy ERC20. This only required for testing
   const erc20Token = await upgrades.deployProxy(erc20Contract, []);
   await erc20Token.deployed();
 
   console.log("Erc20 deployed to:", erc20Token.address);
 
+  // Deploy stake contract.
   const stakeContract = await upgrades.deployProxy(stakeToken, [
     erc20Token.address,
     nft.address,
   ]);
   await stakeContract.deployed();
 
-  console.log("Stake deployed to:", stakeToken.address);
+  console.log("Stake deployed to:", stakeContract.address);
 }
 
 main();
